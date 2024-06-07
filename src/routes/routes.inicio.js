0489a0cb95476a186029cf0def12e7e287a3b347
Angie Nohava <angienohava03@gmail.com>
2024-06-07 16:28:34 -0500

    reserva barberos lista

--- src/routes/routes.inicio.js
+++ src/routes/routes.inicio.js
@@ -4,6 +4,6 @@ import { inicio, reservar } from "../controllers/controller.inicio";
 const rutaInicio = Router();
 
 rutaInicio.get("/", inicio)
-rutaInicio.get("/reservar", reservar)
+rutaInicio.get("/reserva", reservar)
 
 export default rutaInicio;
