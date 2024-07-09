1f44f251295774f41fca78ec62078b810b5e40a2
angienohava03 <angienohava03@gmail.com>
2024-07-08 21:36:15 -0500

    validaciones de registro y login listas

--- src/public/js/calendario.js
+++ src/public/js/calendario.js
@@ -37,18 +37,20 @@ const renderCalendario = () => {
     mes.innerText = `${meses[actualMes]}`;
     ano.innerText = `${actualAno}`;
     diaTag.innerHTML = liTag;
-    
-    liTag.addEventListener('click', () => {
-        let clickedDay = parseInt(day.innerText);
-        let clickedDate = new Date(actualAno, actualMes, clickedDay);
 
-        if (clickedDate < new Date()) {
-            localStorage.setItem('dia', 'true');
-        } else {
-            localStorage.setItem('clickedPastDate', 'false');
-        }
+    // Agregar eventos de click a cada día
+    document.querySelectorAll('.num-dias li').forEach(day => {
+        day.addEventListener('click', () => {
+            let clickedDay = parseInt(day.innerText);
+            let clickedDate = new Date(actualAno, actualMes, clickedDay);
+
+            if (clickedDate < new Date()) {
+                localStorage.setItem('dia', 'true');
+            } else {
+                localStorage.setItem('dia', 'false');
+            }
+        });
     });
-    
 };
 
 renderCalendario();
@@ -78,129 +80,3 @@ prevIcon.forEach(icon => {
         renderCalendario();
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const diaActual = document.querySelector('.dia-actual');
-// const mes = document.querySelector('.mes');
-// const ano = document.querySelector('.ano');
-
-// const diaTag = document.querySelector('.num-dias')
-// const nextIcon = document.querySelectorAll('.btn-next')
-// const prevIcon = document.querySelectorAll('.btn-prev')
-
-// // trae la fecha, el mes, y el año actual 
-// let fecha = new Date();
-// actualAno = fecha.getFullYear();
-// actualMes = fecha.getMonth();
-
-// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
-//     'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-// ];
-
-// const renderCalendario = () => {
-//     // trae el último dia del mes 
-//     let primerDiaMes = new Date(actualAno, actualMes, 1).getDay(), //captura el primer dia del mes
-//     ultimoFechaMes = new Date(actualAno, actualMes + 1, 0).getDate(), //captura ultima fecha del mes
-//     ultimoDiaMes = new Date(actualAno, actualMes, ultimoFechaMes).getDay(), //captura ultimo dia del mes
-//     ultimoFechaUltimoMes = new Date(actualAno, actualMes, 0).getDate() //captura ultima fecha del anterior mes
-//     let liTag = '';
-
-//     for (let i = primerDiaMes; i > 0; i--){
-//         liTag += `<li class="inactivos">${ultimoFechaUltimoMes - i + 1}</li>`     
-//     }
-    
-//     for (let i = 1; i <= ultimoFechaMes ; i++) {
-//         let hoy = i === fecha.getDate() && actualMes === new Date().getMonth() 
-//         && actualAno === new Date().getFullYear() ? 'dia-actual' : "";
-
-        
-//         liTag += `<li id="misDias" class="${hoy}">${i}</li>`
-//     }
-//     for (let i = ultimoDiaMes; i < 6 ; i++) {
-//         liTag += `<li id="01" class="inactivos">${ i - ultimoDiaMes + 1}</li>`     
-//     }
-
-//     mes.innerText = `${meses[actualMes]}`;
-//     ano.innerText =  `${actualAno}`
-//     diaTag.innerHTML = liTag;
-
-//     // Agregar eventos de click a cada día
-//     document.querySelectorAll('.num-dias li').forEach(day => {
-//         day.addEventListener('click', () => {
-//             let clickedDay = parseInt(day.innerText);
-//             let clickedDate = new Date(actualAno, actualMes, clickedDay);
-
-//             if (clickedDate < new Date()) {
-//                 localStorage.setItem('id_dia', 'true');
-//             } else {
-//                 localStorage.setItem('id_dia', 'false');
-//             }
-//         });
-//     });
-// }
-// renderCalendario();
-
-// nextIcon.forEach(icon => {
-//     icon.addEventListener('click', () => {
-//         actualMes = icon.id === "next" ? actualMes + 1 : actualMes - 1;
-
-//         if(actualMes < 0 || actualMes > 11){
-//             fecha = new Date(actualAno, actualMes);
-//             actualAno = fecha.getFullYear();
-//             actualMes = fecha.getMonth();
-//         }else{
-//             fecha = new Date();
-//         }
-
-//         renderCalendario(); 
-//     })
-// });
-
-
-// prevIcon.forEach(icon => {
-//     icon.addEventListener('click', () => {
-//         actualMes = icon.id === "prev" ? actualMes - 1 : actualMes + 1;
-
-//         if(actualMes < 0 || actualMes > 11){
-//             fecha = new Date(actualAno, actualMes);
-//             actualAno = fecha.getFullYear();
-//             actualMes = fecha.getMonth();
-//         }else{
-//             fecha = new Date();
-//         }
-
-//         renderCalendario(); 
-//     })
-// });
--- src/public/js/registro.js
+++ src/public/js/registro.js
@@ -2,17 +2,17 @@
  * Funcion para realizar el registro
  */
 
+
 const registrar = () => {
     const id = document.getElementById('id').value;
     const nombre = document.getElementById('nombre').value;
     const telefono = document.getElementById('telefono').value;
     const correo = document.getElementById('email').value;
     const contra = document.getElementById('contrasena').value;
-    const roll = document.querySelector('#roll').value;
+    const rol = document.querySelector('#rol').value;
 
     const url = sessionStorage.getItem('urlsupra');
     const endpoint = url + "/api/registro"
-
     
     const options = {
         method : 'POST',
@@ -25,16 +25,24 @@ const registrar = () => {
             telefono : telefono,
             correo : correo,
             contrasena : contra,
-            roll : roll
+            roll : rol
         })
     };
     
     fetch(endpoint, options)
     .then(res => res.json())
     .then(data => {
-        if(data.error == true){
-            alertify.error('Algo está errado');
-        } 
+        if(data.status == 402){
+            alertify.error('La cuenta ya ha sido creada');
+        }else{
+            if(data.error == true){
+                alertify.error('Los campos no ha sido completados');
+            }else{
+                alertify.success('Su cuenta ha sido creada');
+                document.querySelector('form').reset();
+            }
+        }
+        
     })
     .catch(err => {
         console.log("Tenemos un problema", err);
@@ -42,19 +50,4 @@ const registrar = () => {
 }
 
 
-/**
- * funcion para la verificaion de los campos del registro
- */
-(function () {
-    'use strict';
-    var forms = document.querySelectorAll('.needs-validation');
-    Array.prototype.slice.call(forms).forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-})();
+
